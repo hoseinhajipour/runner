dfb12a1a0f7c16073d5fa2608357eff278bb1d6d
hosein hajipour <hoseinhajepor@yahoo.com>
2020-10-04 18:16:54 +0330

    hajigames runner

---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotateSpeed = 10f;
+
+    public GameObject CatchEffect;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerManager.numberOfCoins++;
+            FindObjectOfType<AudioManager>().PlaySound("PickUpCoin");
+            Instantiate(CatchEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
+    }
+
+}
